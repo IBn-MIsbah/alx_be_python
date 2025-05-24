48071a727f4b9ef99084881955b19a723c1ffb77
IBn-MIsbah <160459932+IBn-MIsbah@users.noreply.github.com>
2025-05-24 21:20:03 +0300

    fixes on variable name

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -5,6 +5,6 @@ monthly_savings = monthly_income - total_monthly_expens
 
 annual_interest_rate = 0.05
 
-projection_saving = int(monthly_saving * 12 + ( monthly_saving * 12 * annual_interest_rate))
+projection_saving = int(monthly_savings * 12 + ( monthly_savings * 12 * annual_interest_rate))
 
 print(f'Projected savings after one year, with interest, is: {projection_saving}')
