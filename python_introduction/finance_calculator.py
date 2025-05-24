180c7b525bfa8e10d0f3b701f72e87f5fdd7a43d
IBn-MIsbah <160459932+IBn-MIsbah@users.noreply.github.com>
2025-05-24 21:22:51 +0300

    changed the variable name

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,7 +1,7 @@
 monthly_income = float(input('Enter your monthly income: '))
-total_monthly_expens = float(input('Enter your total monthly expenses: '))
+monthly_expens = float(input('Enter your total monthly expenses: '))
 
-monthly_savings = monthly_income - total_monthly_expens
+monthly_savings = monthly_income - monthly_expens
 
 annual_interest_rate = 0.05
 
