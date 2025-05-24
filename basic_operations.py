ceb5dbaf182d6c3f3acaaa8d0337f3010d24a9a3
IBn-MIsbah <160459932+IBn-MIsbah@users.noreply.github.com>
2025-05-24 18:23:43 +0300

    basic python operation

---
+++ basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10
+number2 = 5
+
+addition = number1 + number2
+substraction = number1 - number2
+multiplication = number2 * number1
+
+print(f'Addition of {number1} and {number2} is {addition} ')
+print(f'Subtraction of {number1} and {number2} is {substraction}')
+print(f'Multiplication of {number1} and {number2} is {multiplication}')
