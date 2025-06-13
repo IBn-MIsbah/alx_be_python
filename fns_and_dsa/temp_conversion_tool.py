da9c09cd2c6fa81fbdf83874d62945e52c5c9111
IBn-MIsbah <160459932+IBn-MIsbah@users.noreply.github.com>
2025-06-13 06:45:19 +0300

    fixed spacing and positioning

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,20 +1,18 @@
-
 FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
-
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
 
 def convert_to_celsius(fahrenheit):
     return (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
 
-def  convert_to_fahrenheit(celsius):
+def convert_to_fahrenheit(celsius):
     return (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
 
-temp = float(input('Enter the temperature to convert: '))
-type_of_temp = input('Is this temperature in Celsius or Fahrenheit? (C/F): ')
+temp = float(input("Enter the temperature to convert: "))
+type_of_temp = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").strip().upper()
 
-if(type_of_temp == 'C' or type_of_temp == 'c'):
+if type_of_temp == "C":
     print(f"{temp}°C is {convert_to_fahrenheit(temp)}°F")
-elif(type_of_temp == 'F' or type_of_temp == 'f'):
-    print(f"{temp}°F is { convert_to_celsius(temp)}°C")
+elif type_of_temp == "F":
+    print(f"{temp}°F is {convert_to_celsius(temp)}°C")
 else:
-    print('There is a problem with the type.')
+    print("There is a problem with the type.")
